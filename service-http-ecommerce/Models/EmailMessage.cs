4e926c3e1c07b4f626f0e09ff54fa40b13a547c4
Guilherme Araujo <guilherme@autodoc.com.br>
2021-06-15 22:43:17 -0300

    Add project service-http-ecommerce

---
+++ service-http-ecommerce/Models/EmailMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace service_http_ecommerce.Models
+{
+    public class EmailMessage
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ service-http-ecommerce/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace service_http_ecommerce.Models
+{
+    public class Order
+    {
+        public string orderId { get; set; }
+        public int amount { get; set; }
+        public string email { get; set; }
+    }
+}
